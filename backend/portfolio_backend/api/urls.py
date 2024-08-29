6d08533b34a5000f9e438f26aa6eaa6a958c2b86
kedabudre <abdurehimk77@gmail.com>
2024-08-29 14:29:02 +0200

    thrailing slash set to false

--- backend/portfolio_backend/api/urls.py
+++ backend/portfolio_backend/api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import ContactInfoView, NewsletterSubscriberView
 
 urlpatterns = [
-    path('contact/', ContactInfoView.as_view(), name='contact_info_list'),
-    path('contact/<str:pk>/', ContactInfoView.as_view(), name='contact_info_detail'),
-    path('subscribe/', NewsletterSubscriberView.as_view(), name='newsletter_subscribe_list'),
-    path('subscribe/<str:pk>/', NewsletterSubscriberView.as_view(), name='newsletter_subscribe_detail'),
+    path('contact', ContactInfoView.as_view(), name='contact_info_list'),
+    path('contact/<str:pk>', ContactInfoView.as_view(), name='contact_info_detail'),
+    path('subscribe', NewsletterSubscriberView.as_view(), name='newsletter_subscribe_list'),
+    path('subscribe/<str:pk>', NewsletterSubscriberView.as_view(), name='newsletter_subscribe_detail'),
 ]
