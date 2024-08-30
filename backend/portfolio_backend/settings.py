89bbffe60d50dfa8d5aea30c8801bcc685579a0d
kedabudre <abdurehimk77@gmail.com>
2024-08-30 08:16:40 +0200

    update file

--- backend/portfolio_backend/settings.py
+++ backend/portfolio_backend/settings.py
@@ -25,11 +25,11 @@ DEBUG = os.getenv('DEBUG') == 'True'
 
 # ALLOWED_HOSTS Configuration
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['nexabackend.vercel.app', '127.0.0.1', 'localhost']
 
-vercel_url = os.environ.get('VERCEL_URL')
-if vercel_url:
-    ALLOWED_HOSTS.append(vercel_url)
+# vercel_url = os.environ.get('VERCEL_URL')
+# if vercel_url:
+#     ALLOWED_HOSTS.append(vercel_url)
 
 
 # Application definition
