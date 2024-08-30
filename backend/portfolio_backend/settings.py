e7a16b9b339789bc35cad4091f5915c3898da684
kedabudre <abdurehimk77@gmail.com>
2024-08-30 09:50:18 +0200

    update file

--- backend/portfolio_backend/settings.py
+++ backend/portfolio_backend/settings.py
@@ -4,23 +4,13 @@ Django settings for portfolio_backend project.
 
 import os
 import mongoengine
-from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Define BASE_DIR using pathlib
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Static files (CSS, JavaScript, Images)
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
@@ -137,4 +127,9 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+
+STATIC_URL = '/static/'
 
