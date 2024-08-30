f0332b48d46a2dbc55dbd00bdac2d4b6b71b8aa0
kedabudre <abdurehimk77@gmail.com>
2024-08-30 09:37:51 +0200

    update file

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Check for default Python version available
-python3 --version
-pip3 --version
-
 # Install dependencies
-pip3 install --upgrade pip
-pip3 install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
 
 # Collect static files if Django is configured
 if [ -f "manage.py" ]; then
-  python3 manage.py collectstatic --noinput
+  python manage.py collectstatic --noinput
 fi
