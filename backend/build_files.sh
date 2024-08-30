6dcbcdca71b222c41b3007a335ffb8ea64967e64
kedabudre <abdurehimk77@gmail.com>
2024-08-30 09:40:30 +0200

    update file

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -5,6 +5,6 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 # Collect static files if Django is configured
-if [ -f "manage.py" ]; then
-  python manage.py collectstatic --noinput
-fi
+# if [ -f "manage.py" ]; then
+#   python manage.py collectstatic --noinput
+# fi
